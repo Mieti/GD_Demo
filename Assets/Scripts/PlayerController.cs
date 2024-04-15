10ab92f67c7037d7fe18e223de447fa13a5676ce
Riccardo Vukovic <riccardo.vukovic@gmail.com>
2024-04-15 23:37:15 +0200

    first commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.LowLevel;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Vector2 moveInput;
+    Rigidbody2D rb;
+    public bool isMoving { get; private set; }
+    [SerializeField] private float walkSpeed = 5f;
+
+    public void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>(); 
+    }
+
+    void Start() 
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveInput.x * walkSpeed, moveInput.y * walkSpeed);
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+        isMoving = moveInput != Vector2.zero;
+    }
+}
