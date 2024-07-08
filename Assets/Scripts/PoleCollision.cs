6b42e21f7724fecf9f92c400162805e1b699f765
Lara Moresco <lara.moresco2000@gmail.com>
2024-07-08 11:33:38 +0200

    fixed poles flickering

--- Assets/Scripts/PoleCollision.cs
+++ Assets/Scripts/PoleCollision.cs
@@ -6,6 +6,7 @@ public class PoleCollision : MonoBehaviour
 {
     // variable used by external objects to trigger events
     public bool active = false;
+    private int colliderCount = 0;
 
     private Dictionary<string, Color> tagColorMapping = new Dictionary<string, Color>
     {
@@ -13,17 +14,15 @@ public class PoleCollision : MonoBehaviour
         { "WrongPole", Color.red }
     };
 
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        colliderCount++;
+        ChangeColorIfNeeded();
+    }
+    // Maybe not needed with the counter
     private void OnTriggerStay2D(Collider2D other)
     {
-        foreach (var tagColor in tagColorMapping)
-        {
-            if (gameObject.tag.Contains(tagColor.Key))
-            {
-                active = true;
-                GetComponent<Renderer>().material.color = tagColor.Value;
-                return;
-            }
-        }
+        ChangeColorIfNeeded();
         /*
         // use tag to discriminate what is currently touching the collider
         if (other.tag == "cable")
@@ -35,8 +34,26 @@ public class PoleCollision : MonoBehaviour
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        active = false;
-        GetComponent<Renderer>().material.color = Color.white;
+        colliderCount--;
+        if (colliderCount <= 0)
+        {
+            colliderCount = 0;
+            active = false;
+            GetComponent<Renderer>().material.color = Color.white;
+        }
+    }
+
+    private void ChangeColorIfNeeded()
+    {
+        foreach (var tagColor in tagColorMapping)
+        {
+            if (gameObject.tag.Contains(tagColor.Key))
+            {
+                active = true;
+                GetComponent<Renderer>().material.color = tagColor.Value;
+                return;
+            }
+        }
     }
 
 }
